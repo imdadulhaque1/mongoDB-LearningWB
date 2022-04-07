d0d8469373462d8546b82fd1147e40125d80baa0
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-04-07 14:14:31 +0600

    Customized Created students using async and await, Read the stored data from mongoDB

--- index.js
+++ index.js
@@ -1,43 +1,61 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/my-students')
-    .then(() =>{
-        console.log("Connected to MongoDB Successfully!");
-    })
-    .catch(err =>{
-        console.error("Connection Failed!")
-    })
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/my-students")
+  .then(() => {
+    console.log("Connected to MongoDB Successfully!");
+  })
+  .catch((err) => {
+    console.error("Connection Failed!");
+  });
 
+//========> Schema -> defines the shape documents
+const studentSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  dob: Date,
+  entryDate: { type: Date, default: Date.now },
+  passStatus: Boolean,
+  hobbies: [],
+  parents: {
+    fatherName: String,
+    motherName: String,
+  },
+  subjects: [{ name: String, marks: { type: Number, min: 0, max: 100 } }],
+});
 
-    //========> Schema -> defines the shape documents
-    const studentSchema = new mongoose.Schema({
-        firstName:String,
-        lastName:String,
-        dob:Date,
-        entryDate: { type:Date, default: Date.now},
-        passStatus: Boolean,
-        hobbies: [],
-        parents: {
-            fatherName: String,
-            motherName: String
-        },
-        subjects: [{name:String, marks:{type:Number, min:0, max:100}}]
-    })
+//========> Model Create
+const Student = mongoose.model("Student", studentSchema);
 
-    //========> Model Create
-    const Student = mongoose.model('Student', studentSchema); 
-    const student = new Student({
-        firstName: "Imdadul",
-        lastName: "Haque",
-        dob: new Date("07 April 2022"),
-        passStatus: true,
-        hobbies: ["Swimming", "Playing Football", "Travelling"],
-        parents:{
-            fatherName: "Ismail Hossain",
-            motherName: "Marzia Khatun"
-        },
-        subjects:[{name: "Computer Fundamental", marks:75}, {name:"Data Science", marks: 77}, {name: "Engineering Mathematics"}]
-    })
-    student.save()
-        .then(studentsData =>{
-            console.log(studentsData);
-        })
+//=====> C = Create
+async function createStudent() {
+  const student = new Student({
+    firstName: "Enamul",
+    lastName: "Haque",
+    dob: new Date("07 April 2022"),
+    passStatus: true,
+    hobbies: ["Swimming", "Playing Football", "Travelling"],
+    parents: {
+      fatherName: "Ismail Hossain",
+      motherName: "Marzia Khatun",
+    },
+    subjects: [
+      { name: "Computer Fundamental", marks: 75 },
+      { name: "Data Science", marks: 77 },
+      { name: "Engineering Mathematics" },
+    ],
+  });
+  try {
+    const studentData = await student.save();
+    console.log(studentData);
+  } catch (err) {
+    console.log(err._message);
+  }
+}
+// createStudent();
+
+//=====> R = Read the created collections
+async function readStudentsInfo() {
+  const studentsData = await Student.find();
+  console.log(studentsData);
+}
+readStudentsInfo();
